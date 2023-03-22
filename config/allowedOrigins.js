66486d9fbb0aba88de1966c7e6196da7d697a70f
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-22 20:39:24 +0800

    Add netlify domain.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://mern-demo-backend.herokuapp.com/',
+    'https://main--deluxe-snickerdoodle-7fa1f9.netlify.app/',
     'http://localhost:4000',
     'http://localhost:3000'
 ];
