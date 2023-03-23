8f8a69da16b3bb5fed5d42f3e3548adbd1c2beee
Paul Figueroa <pjsfigueroa@gmail.com>
2023-03-23 20:15:31 +0800

    Update allowed origins, update netlify url.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://main--exquisite-griffin-20cac4.netlify.app',
+    'https://exquisite-griffin-20cac4.netlify.app/',
     'https://mern-demo-backend.herokuapp.com',
     'http://localhost:4000',
     'http://localhost:3000'
